c98a7f0f45a26a8db2b4230f5efc3385ef1af893
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-10-26 14:52:45 -0400

    Release: 1.0.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="git-commitflow",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="A git add/commit/push helper",
     long_description=((Path(__file__).parent.resolve().joinpath("README.md"))
