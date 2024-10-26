1d5e82bab736fc952665fe182762b9b51617243e
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-10-26 14:36:34 -0400

    Update docstrings

--- git_commitflow/git_commitflow.py
+++ git_commitflow/git_commitflow.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see <https://www.gnu.org/licenses/>.
 #
-"""Readline manager."""
+"""Git commitflow command-line interface."""
 
 import argparse
 import logging
